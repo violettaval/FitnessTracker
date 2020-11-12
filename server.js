cb11280c1cd39f5426eb41bca021bbfd09b7b966
Violet <violetvalencia37@gmail.com>
2020-11-12 00:14:19 -0600

    typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fittnesstracker',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
